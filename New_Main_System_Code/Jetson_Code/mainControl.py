e03cecfab6d6c5d12d29e30f7d68ba2ca550bade
ArunanElamaran <arunan.elamaran@gmail.com>
2024-03-23 20:06:24 -0700

    Completed mainControl.py

--- New_Main_System_Code/Jetson_Code/mainControl.py
+++ New_Main_System_Code/Jetson_Code/mainControl.py
@@ -1,5 +1,5 @@
 import multiprocessing, threading
-import os
+import os, glob
 import subprocess
 
 '''
@@ -57,57 +57,62 @@ def User_Environment_Input():
 
 # -------------- File-Procesisng Process Functions --------------
     
-def file_Monitoring_And_Handling():
-    # Check if the file exists
-    file_index = 0
-    while (True):
-        file_path = f'{file_folder}/subtask_{file_index}'
-        if os.path.exists(file_path):
-            completed_process = subprocess.run(["python3", file_path])
-            if completed_process.returncode == 0:
-                file_index += 1
-            else:
-                exit()
-
-def file_Execution():
-    return
-
 def File_Processing():
     '''
-    - The microphone constantly listens for user commands/instructions
-    - Speech to text conversion
+    - Executes each received python file based on chronological order of sending
+    - If exit file received, immediately terminates current process
     '''
 
-    recording_thread = threading.thread(target=recording)
-    speechProcessing_thread = threading.thread(target=speech_Processing)
+    main_path = os.path.abspath(__file__); main_path = main_path[:main_path.rfind("/")] + "/codeFiles/"  # Path in which code files are received
+    file_process = subprocess.Popen([":"], shell=True) # Shell command : does nothing
+    file_number = 1
 
-    # Start the threads
-    recording_thread.start()
-    speechProcessing_thread.start()
+    while (True):
+        
+        # Check to see if an exit file exists
+        if("exit.py" in os.listdir(main_path)): 
+            # If a subtask is currently executing, terminate the process
+            if (file_process.poll() != None): file_process.terminate()
+            # Create list of all files in the code folder, sorted by creation time
+            files = list(filter(os.path.isfile, glob.glob(main_path+"/*.py")))
+            files.sort(key=os.path.getctime)
+            exit_index = files.index(main_path+"exit.py")
+            # Delete all the files on the system before it (except for mainControl.py)
+            for file in files[:exit_index+1]:
+                os.remove(main_path+file)
+            # Reset file count number to 1
+            file_number = 1
+        
+        # If the current subtask not done executing, continue
+        if(file_process.poll() == None): continue
 
-    # Wait for both threads to finish (which will never occur unless program is forcefully stopped or crashes)
-    recording_thread.join()
-    speechProcessing_thread.join()
-
-    return
+        # Check to see if the current file number exists. If does, execute and increment file_number
+        desired_subtask_file = "subtask_"+str(file_number)+".py"
+        if(desired_subtask_file in os.listdir(main_path)):
+            file_process = subprocess.Popen(["python3", desired_subtask_file])
+            if file_number >= 255: file_number = 1
+            else: file_number+=1
 
 
 # -------------- Main Parent Process --------------
 def main_process():
-    # Define the file path for subtasks
-    file_folder = "~/Main_System_Code/JetsonCode/subtasks"
-    
-    # Create threads for each process
-    thread1 = multiprocessing.Process(target=User_Environment_Input)
-    thread2 = multiprocessing.Process(target=File_Processing)
 
-    # Start the threads
-    thread1.start()
-    thread2.start()
+    # Create process for each process
+    User_Environment_Input_Process = multiprocessing.Process(target=User_Environment_Input)
+    File_Processing_Process = multiprocessing.Process(target=File_Processing)
 
-    # Wait for both threads to finish
-    thread1.join()
-    thread2.join()
+    # Start the process
+    User_Environment_Input_Process.start()
+    File_Processing_Process.start()
+
+    # Wait for both process to finish
+    User_Environment_Input_Process.join()
+    File_Processing_Process.join()
 
 if __name__ == "__main__":
     main_process()
+
+'''
+Extra Notes:
+Threads are smaller/cheaper than processes, since threads share the same memory whereas processes each have their own copy. So might use split into two threads when doing a task that is relatively short/requires a lot of sharing of data, whereas you might split into two processes for longer, more independent tasks
+'''
